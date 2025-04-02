6040ab8ec3a8564d2a3fa69af308dd02925dcf1c
Boris Vasilev <borisonekenobi@gmail.com>
2025-04-02 04:23:57 -0400

    Add random UUID generator to utils.h

--- utils.h
+++ utils.h
@@ -3,12 +3,14 @@
 #include <string>
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/random_generator.hpp>
 
 using namespace std;
 
 #ifndef UTILS_H
 #define UTILS_H
 
+inline boost::uuids::random_generator gen_uuid;
 bool is_valid_uuid(const string& str, boost::uuids::uuid& result);
 
 #endif //UTILS_H
