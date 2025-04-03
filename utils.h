125b4f6c62213a6db8ac1ab04fd4cfd6cd73cb37
Boris Vasilev <borisonekenobi@gmail.com>
2025-04-03 05:21:07 -0400

    Refactor UUID validation: change string type to std::string and remove unnecessary using directive

--- utils.h
+++ utils.h
@@ -8,8 +8,6 @@
 
 #include <nlohmann/json.hpp>
 
-using namespace std;
-
 inline boost::uuids::random_generator gen_uuid;
-bool is_valid_uuid(const string& str, boost::uuids::uuid& result);
+bool is_valid_uuid(const std::string& str, boost::uuids::uuid& result);
 bool is_valid_token_data(const nlohmann::basic_json<>& data);
